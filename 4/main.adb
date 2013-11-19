5b92705bf9eff0323a0193f47a54e8a062e0a3f6
dubst3pp4 <dubst3pp4@googlemail.com>
2013-11-19 15:05:05 +0100

    added files for problem 4

--- 4/main.adb
+++ 4/main.adb
@@ -1,24 +1,81 @@
 ---------------------------------------------------------------------------
--- Largest palindrome product
--- Problem 4
---
--- A palindromic number reads the same both ways. The largest palindrome 
--- made from the product of two 2-digit numbers is 9009 = 91 × 99.
--- Find the largest palindrome made from the product of two 3-digit numbers.
+-- Largest palindrome product                                            --
+-- Problem 4                                                             --
+--                                                                       --
+-- A palindromic number reads the same both ways. The largest palindrome --
+-- made from the product of two 2-digit numbers is 9009 = 91 × 99.       --
+-- Find the largest palindrome made from the product of two 3-digit      --
+-- numbers.                                                              --
 ---------------------------------------------------------------------------
 
 with Ada.Text_IO;
+with Ada.Strings.Fixed;
 
 procedure Main is
 
-   function Is_Palindrome( Item : String ) return Boolean is
+   package IO renames Ada.Text_IO;
+   package SF renames Ada.Strings.Fixed;
+
+   function Reverse_String( Item : String ) return String is
+      Item_Reverse : String( Item'Range );
    begin
+      for I in Item'Range loop
+         Item_Reverse( Item_Reverse'Last - I + Item'First ) := Item( I );
+      end loop;
+      return Item_Reverse;
+   end Reverse_String;
+
+   function Is_Palindrome( Item : String ) return Boolean is
+      Half_Length        : constant Integer := Integer( Float'Floor( Float(Item'Length) / Float(2) ) );
+      Part_A             : constant String  := Item( Item'First..Half_Length );
+      Part_A_Lower_Bound : constant Integer := Item'Last - Half_Length;
+      Part_B             : constant String  := Item( Part_A_Lower_Bound+1..Item'Last );
+   begin
+      if Half_Length = 1 then
+         return true;
+      end if;
+      if Part_A = Reverse_String( Part_B ) then
+         return true;
+      end if;
       return false;
    end Is_Palindrome;
 
+   Max_Factor         : constant Integer := 999;
+   Factor_A           : Integer          := 0;
+   Factor_B           : Integer          := 0;
+   Largest_Palindrome : Integer          := 0;
+   Found              : Boolean          := false;
 
 begin
 
-   null;
+   A_Loop:
+   for I in reverse 100..Max_Factor loop
+
+      Factor_A := I;
+
+      B_Loop:
+      for J in reverse 100..Max_Factor loop
+
+         Factor_B := J;
+         Largest_Palindrome := I * J;
+         Found := Is_Palindrome( 
+            SF.Trim( Integer'Image( Largest_Palindrome ), Ada.Strings.Left) 
+         );
+
+         exit A_Loop when Found = true;
+      end loop B_Loop;
+
+   end loop A_Loop;
+
+   if Found = true then
+      IO.Put_Line( "the largest palindrom made by a product of 2 three-digit numbers is"
+                   & " the product of"
+                   & Integer'Image(Factor_A) & " *" & Integer'Image(Factor_B)
+                   & " ="
+                   & Integer'Image( Largest_Palindrome ) 
+      );
+   else
+      IO.Put_Line( "could not find the largest palindrome." );
+   end if;
 
 end Main;
